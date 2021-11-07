79a8098e7425deac6cc44afbfafc3171b0641db2
khoakim <khoakim@atosp.onmicrosoft.com>
2021-11-07 16:21:22 +1100

    beautify

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { StatusBar } from "expo-status-bar";
+import {StatusBar} from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
+import {StyleSheet, Text, View, Button} from "react-native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
 
 import CreateScreen from "./src/screens/CreateScreen";
 import EditScreen from "./src/screens/EditScreen";
@@ -19,15 +19,17 @@ export default function App() {
           <Stack.Screen
             name="Create"
             component={CreateScreen}
-            options={{ title: "Overview" }}
+            options={{
+              title: "Overview",
+            }}
           />
           <Stack.Screen name="Edit" component={EditScreen} />
           <Stack.Screen name="Show" component={ShowScreen} />
           <Stack.Screen
             name="Index"
             component={IndexScreen}
-            options={({ navigation }) => ({
-              headerTitle: props => <Text>asdfa</Text>,
+            options={({navigation}) => ({
+              headerTitle: (props) => <Text>asdfa</Text>,
               headerRight: () => (
                 <Button
                   onPress={() => navigation.navigate("Create")}
